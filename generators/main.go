9035400686d7a0777e59c093a077b27d4e0e8e58
sreecharan <charan.24119@gmail.com>
2024-01-16 14:39:20 +0000

    fan In and fan out concepts

--- generators/main.go
+++ generators/main.go
@@ -137,3 +137,31 @@ func main() {
 	// 	fmt.Println(rando)
 	// }
 }
+
+
+// func worker(id int, jobs <-chan int, results chan<- int) {
+// 	for j := range jobs {
+// 		fmt.Println("worker", id, "processing job", j)
+// 		results <- j * 2
+// 	}
+// }
+
+// func main() {
+// 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+// 	jobs := make(chan int, 100)
+// 	results := make(chan int, 100)
+
+// 	for w := 0; w <= runtime.NumCPU(); w++ {
+// 		go worker(w, jobs, results)
+// 	}
+
+// 	for j := 1; j <= 9; j++ {
+// 		jobs <- j
+// 	}
+// 	close(jobs)
+
+// 	for a := 1; a <= 9; a++ {
+// 		<-results
+// 	}
+// }
